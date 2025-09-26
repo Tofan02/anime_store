9da8cc3e6bb631adce51ee70aa93310c0ad54827
Tofan02 <lenovokua2010a@gmail.com>
2025-09-26 22:18:31 +0800

    Memperbaiki data anime yang tidak update

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ let currentPage = 1;
 let itemsPerPage = parseInt(itemsPerPageSelect.value);
 
 // Load CSV
-fetch("data/anime.csv")
+fetch("data/anime-26092025.csv")
   .then((response) => response.text())
   .then((csvText) => {
     const results = Papa.parse(csvText, {
