802d2aa872b8bc48264b87f548fb040a154d372e
Tofan02 <lenovokua2010a@gmail.com>
2025-09-28 21:29:37 +0800

    Memperbaiki format size yang salah pada Dr. Stone; Science Future Part  2

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ let currentPage = 1;
 let itemsPerPage = parseInt(itemsPerPageSelect.value);
 
 // Load CSV
-fetch("data/anime-28092025.csv")
+fetch("data/anime-28092025(1).csv")
   .then((response) => response.text())
   .then((csvText) => {
     const results = Papa.parse(csvText, {
