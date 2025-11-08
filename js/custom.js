3407e0189538c3098272783600ddcc367e995cf2
Tofan02 <lenovokua2010a@gmail.com>
2025-11-08 14:58:28 +0800

    Update Anime Baru

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ let currentPage = 1;
 let itemsPerPage = parseInt(itemsPerPageSelect.value);
 
 // Load CSV
-fetch("data/anime-03112025.csv")
+fetch("data/anime-08112025.csv")
   .then((response) => response.text())
   .then((csvText) => {
     const results = Papa.parse(csvText, {
