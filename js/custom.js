95efffe035f7aafca7508926bbf45eb92c143839
lyntom <lyntom.irfan.d@gmail.com>
2026-04-28 22:35:04 +0800

    Update Anime Baru

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ let currentPage = 1;
 let itemsPerPage = parseInt(itemsPerPageSelect.value);
 
 // Load CSV
-fetch("data/anime-19042026.csv")
+fetch("data/anime-28042026.csv")
   .then((response) => response.text())
   .then((csvText) => {
     const results = Papa.parse(csvText, {
